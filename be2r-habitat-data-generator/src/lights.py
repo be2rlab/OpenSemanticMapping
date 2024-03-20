e09582913b92337951a4b565bd6d32f626433d0b
Maxim Popov <max.popov.02@ya.ru>
2024-03-21 01:58:09 +0300

    lights parcing update

--- be2r-habitat-data-generator/src/lights.py
+++ be2r-habitat-data-generator/src/lights.py
@@ -1,34 +1,38 @@
+import numpy as np
+
 from habitat_sim.gfx import LightInfo, LightPositionModel
 
 from utils.settings import make_cfg
 
 
 default_light_settings = {
-    'lights': [
-        {
-            'info': {
-                'vector': [0., 0., 0., 1.0],
-                'color': [10.0, .0, .0],
-                'model': 'camera',
-            },
-            'life_interval': [None, 1]    
+    'lights': {
+        '0': {
+            'position': [0., 0., 0., 1.0],
+            'intensity': 10.0,
+            'color': [1.0, .0, .0],
+            'type': 'camera',
+            'life_intervals': [
+                [None, 1]
+            ]
         },
-        {
-            'info': {
-                'vector': [0., 0., 0., 1.0],
-                'color': [.0, 10.0, .0],
-                'model': 'camera',
-            },
-            'life_interval': [1, None]    
-        }
-    ]
+        '1': {
+            'position': [0., 0., 0., 1.0],
+            'intensity': 10.0,
+            'color': [.0, 1.0, .0],
+            'type': 'camera',
+            'life_intervals': [
+                [1, None]
+            ]
+        },
+    }
 }
 
 
-light_models_mapping = {
+light_types_mapping = {
     'camera': LightPositionModel.Camera,
-    'global': LightPositionModel.Global,
-    'object': LightPositionModel.Object,
+    'point': LightPositionModel.Global,
+    'directional': LightPositionModel.Object,
 }
 
 
@@ -36,16 +40,27 @@ def set_light_setup(sim, settings, life_index):
     setup = []
     setup_name = f'lights_setup_{life_index}'
 
-    for setup_params in settings['lights']:
-        interval = setup_params['life_interval']
+    for setup_id, setup_params in settings['lights'].items():
+        intervals = setup_params['life_intervals']
 
-        if (interval[0] is None or interval[0] <= life_index) and (interval[1] is None or life_index < interval[1]):
-            info = setup_params['info'].copy()
-            info['model'] = light_models_mapping[info['model']]
-            setup.append(info)
+        for interval in intervals:
+            if (interval[0] is None or interval[0] <= life_index) and (interval[1] is None or life_index < interval[1]):
+                intens_koef = 0.1 if setup_params['intensity'] < 0 else 2.5
+                info = {
+                    'model': light_types_mapping[setup_params['type']],
+                    'vector': setup_params['position'],
+                    'color': list(np.clip(setup_params['color'], 0.0, 1.0) * setup_params['intensity'] * intens_koef)
+                }
+
+                if len(info['vector']) == 3:
+                    info['vector'] += [1.0]
+
+                setup.append((setup_id, info))
+
+                break
 
     sim.set_light_setup(
-        [LightInfo(**info) for info in setup], 
+        [LightInfo(**info) for (_, info) in setup], 
         setup_name
     )
 
@@ -53,6 +68,9 @@ def set_light_setup(sim, settings, life_index):
 
 
 def change_lights(sim, sim_settings, light_settings, life_index):
+    if not sim_settings['override_scene_light_defaults']:
+        return sim, None
+    
     sim, setup_name, setup = set_light_setup(sim, light_settings, life_index)
 
     sim_settings = sim_settings.copy()
